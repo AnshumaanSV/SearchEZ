648ae182ff7ce04ddc0aee4180458149710f7b1a
Anshumaan SV <anshumaansv@gmail.com>
2018-12-26 22:02:30 +0530

    updated file

--- Main.java
+++ Main.java
@@ -1,97 +1,194 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.Desktop.*;
+import java.awt.Image.*;
+import java.net.*;
 
-class searchURL extends Frame implements WindowListener, ActionListener
+import javax.imageio.ImageIO;
+
+class searchURL extends Frame implements WindowListener, ActionListener 
 {
-    Label searchLabel = new Label("Enter a product");
+    Label searchLabel = new Label(" Enter a product");
     TextField searchField = new TextField(50);
     Button searchButton = new Button("Search");
 
-    Label AmazonLabel = new Label("Amazon");
-    TextField AmazonField = new TextField(70);
-    Label FlipkartLabel = new Label("Flipkart ");
-    TextField FilpkartField = new TextField(70);
-    Label EbayLabel = new Label("Ebay     ");
-    TextField EbayField = new TextField(70);
+    Button AmazonButton = new Button("Amazon");
+    Button FlipkartButton = new Button("Flipkart");
+    Button EbayButton = new Button("Ebay");
+    Button ShopcluesButton = new Button("Shopclues");
+    Button SnapdealButton = new Button("Snapdeal");
+
+    Image Background = Toolkit.getDefaultToolkit().createImage("Resources/BG.jpg");
 
     searchURL()
     {
         setLayout(new FlowLayout());                          //Frame
+
         add(searchLabel);                                     //Adding the components to frame
         add(searchField);
         add(searchButton);
 
-        add(AmazonLabel); add(AmazonField);
-        add(FlipkartLabel); add(FilpkartField);
-        add(EbayLabel); add(EbayField);
+        add(AmazonButton);
+        add(FlipkartButton);
+        add(EbayButton);
+        add(ShopcluesButton);
+        add(SnapdealButton);
 
         setTitle("SearchEZ!");                                //Naming the fram and setting its size
-        setSize(640, 200);        
+        setSize(640, 100);        
 
         searchField.setEditable(true);                        //Lets the user to type in TextField
-        searchButton.addActionListener(this);
+        
+        searchButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                input = searchField.getText();
+            }
+        });
+
+        AmazonButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try 
+                {
+                    Desktop.getDesktop().browse(new URL(Amazon()).toURI());
+                } 
+                catch (Exception E) {}
+            }
+        });
+
+        FlipkartButton.addActionListener(new ActionListener()
+        {   
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try 
+                {
+                    Desktop.getDesktop().browse(new URL(Flipkart()).toURI());
+                } 
+                catch (Exception E) {}
+            }
+        });
+
+        EbayButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try 
+                {
+                    Desktop.getDesktop().browse(new URL(Ebay()).toURI());
+                } 
+                catch (Exception E) {}
+            }
+        });
+
+        ShopcluesButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try 
+                {
+                    Desktop.getDesktop().browse(new URL(Shopclues()).toURI());
+                } 
+                catch (Exception E) {}
+            }
+        });
+
+        SnapdealButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                try 
+                {
+                    Desktop.getDesktop().browse(new URL(Snapdeal()).toURI());
+                } 
+                catch (Exception E) {}
+            }
+        });
 
         addWindowListener(this);                              //Window listener to close the window
 
+        setResizable(false);
         setVisible(true);                                     //Letting the frame display
     }
+    
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        g.drawImage(Background, 0, 0, this);
+    }
 
-    //Hard coded methods to create the url of the search product
+    // Hard coded methods to create the url of the search product
     String input;
 
-    public String Amazon()
+    public String Amazon() 
     {
-        //Input spaces to be replaced with "+" and placed after url
         String url = "https://www.amazon.in/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=";
         String result = input.replace(" ", "+");
-        
-        return url+result;
+
+        return url + result;
     }
 
-    public String Flipkart()
+    public String Flipkart() 
     {
-        //Input spaces to be replaced with "%20" and placed after url1 and before url2
         String url1 = "https://www.flipkart.com/search?q=";
         String url2 = "&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off";
-        String result = input.replace(" ","%20");
+        String result = input.replace(" ", "%20");
 
-        return url1+result+url2;
+        return url1 + result + url2;
     }
 
-    public String Ebay()
+    public String Ebay() 
     {
-        //Input spaces to be replaced with "+" and placed after url1 and before url2
         String url1 = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=";
         String url2 = "&_sacat=0";
         String result = input.replace(" ", "+");
 
-        return url1+result+url2;
+        return url1 + result + url2;
     }
 
-    public void actionPerformed(ActionEvent e)
+    public String Shopclues()
     {
-        input = searchField.getText();
+        String url1 = "https://www.shopclues.com/search?q=";
+        String url2 = "&sc_z=3333&z=1&count=10";
+        String result = input.replace(" ", "%20");
 
-        AmazonField.setText(Amazon());
-        FilpkartField.setText(Flipkart());
-        EbayField.setText(Ebay());
+        return url1 + result + url2;
+    }
+
+    public String Snapdeal()
+    {
+        String url1 = "https://www.snapdeal.com/search?keyword=";
+        String url2 = "&santizedKeyword=&catId=&categoryId=0&suggested=false&vertical=&noOfResults=20&searchState=&clickSrc=go_header&lastKeyword=&prodCatId=&changeBackToAll=false&foundInAll=false&categoryIdSearched=&cityPageUrl=&categoryUrl=&url=&utmContent=&dealDetail=&sort=rlvncy";
+        String result = input.replace(" ", "%20");
+
+        return url1 + result + url2;
     }
 
     public void windowClosing(WindowEvent e)
     {
         System.exit(0);
     }
-    public void windowOpened(WindowEvent evt) {}
-    public void windowClosed(WindowEvent evt) {}
-    public void windowDeactivated(WindowEvent evt) {}
-    public void windowActivated(WindowEvent evt) {}
-    public void windowDeiconified(WindowEvent evt) {}
-    public void windowIconified(WindowEvent evt) {}
+
+    public void actionPerformed(ActionEvent e) {}
+    public void windowOpened(WindowEvent e) {}
+    public void windowClosed(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
 }
 
-public class Main
-{
-    public static void main(String[] args) 
+public class Main 
+{  
+     public static void main(String[] args) 
     {
         searchURL search = new searchURL();
     }
